d04cc2cc8142a8030ee13367509970797659d670
Vasudha Singh <vasusingh.1331@gmail.com>
2025-04-20 20:07:46 +0530

    Add logger and exception

---
+++ setup.py
@@ -0,0 +1,28 @@
+from setuptools import setup, find_packages
+from typing import List
+
+HYPHEN_E_DOT = "-e ."
+
+
+def get_requirements(file_path: str) -> List[str]:
+    """
+    This function returns a list of requirements from the given file path.
+    """
+    requirements = []
+    with open(file_path) as file_obj:
+        requirements = file_obj.readlines()
+        requirements = [req.replace("\n", "") for req in requirements]
+
+        if HYPHEN_E_DOT in requirements:
+            requirements.remove(HYPHEN_E_DOT)
+    return requirements
+
+
+setup(
+    name="ml_report",
+    version="0.1.0",
+    author="Vasudha",
+    author_email="vasusingh.1331@gmail.com",
+    packages=find_packages(),
+    install_requires=get_requirements('requirements.txt')
+)
